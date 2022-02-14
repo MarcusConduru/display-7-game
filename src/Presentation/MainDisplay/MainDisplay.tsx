0e9fc0ff086843edc8272bb367fc0566f897c348
MarcusConduru <marcus.conduru@gmail.com>
2022-02-14 10:23:26 -0300

    feat: initial project config

---
+++ src/Presentation/MainDisplay/MainDisplay.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const MainDisplay: React.FC = () => {
+  return (
+    <div>oi</div>
+  )
+}
+
+export default MainDisplay;
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MainDisplay from './Presentation/MainDisplay/MainDisplay';
+import './Globals/styles.scss'
+
+ReactDOM.render(<MainDisplay />,document.getElementById('root'));
